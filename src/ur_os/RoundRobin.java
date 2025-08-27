5c98505709fe3bc2a2f0990090ae1dc93dac62eb
QueenR1014 <129343853+QueenR1014@users.noreply.github.com>
2025-08-26 20:26:06 -0500

    Initial commit

---
+++ src/ur_os/RoundRobin.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os;
+
+/**
+ *
+ * @author prestamour
+ */
+public class RoundRobin extends Scheduler{
+
+    int q;
+    int cont;
+    boolean multiqueue;
+    
+    RoundRobin(OS os){
+        super(os);
+        q = 5;
+        cont=0;
+    }
+    
+    RoundRobin(OS os, int q){
+        this(os);
+        this.q = q;
+    }
+
+    RoundRobin(OS os, int q, boolean multiqueue){
+        this(os);
+        this.q = q;
+        this.multiqueue = multiqueue;
+    }
+    
+
+    
+    void resetCounter(){
+        cont=0;
+    }
+   
+    @Override
+    public void getNext(boolean cpuEmpty) {
+        //Insert code here
+    }
+    
+    
+    @Override
+    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
+    
+}
---
+++ src/ur_os/SJF_P.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author prestamour
+ */
+public class SJF_P extends Scheduler{
+
+    
+    SJF_P(OS os){
+        super(os);
+
+    }
+    
+    @Override
+    public void newProcess(boolean cpuEmpty){// When a NEW process enters the queue, process in CPU, if any, is extracted to compete with the rest
+        
+    } 
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty){// When a process return from IO and enters the queue, process in CPU, if any, is extracted to compete with the rest
+        
+    } 
+    
+   
+    @Override
+    public void getNext(boolean cpuEmpty) {
+        
+        //Insert code here
+
+     }
+ 
+}
