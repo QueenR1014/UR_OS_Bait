55f4a5902321adf553a88793d8d22c8cdcf963a4
DANIEL ALFONSO NIETO NARANJO <danielal.nieto@urosario.edu.co>
2025-09-09 22:15:31 -0500

    Update MFQ.java

--- src/ur_os/MFQ.java
+++ src/ur_os/MFQ.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
  */
 public class MFQ extends Scheduler{
 
-    int currentScheduler;
+    private int currentScheduler;
     
     private ArrayList<Scheduler> schedulers;
     //This may be a suggestion... you may use the current sschedulers to create the Multilevel Feedback Queue, or you may go with a more tradicional way
@@ -22,24 +22,34 @@ public class MFQ extends Scheduler{
     MFQ(OS os){
         super(os);
         currentScheduler = -1;
-        schedulers = new ArrayList();
+        schedulers = new ArrayList<>();
     }
     
     MFQ(OS os, Scheduler... s){ //Received multiple arrays
         this(os);
         schedulers.addAll(Arrays.asList(s));
-        if(s.length > 0)
+        if(!schedulers.isEmpty())
             currentScheduler = 0;
     }
         
     @Override
     public void addProcess(Process p){
        //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
-        
+       if(!schedulers.isEmpty()){
+           schedulers.get(0).addProcess(p);
+           defineCurrentScheduler();
+       }
     }
     
     void defineCurrentScheduler(){
         //This methos is siggested to help you find the scheduler that should be the next in line to provide processes... perhaps the one with process in the queue?
+        currentScheduler = -1;
+        for(int i=0; i<schedulers.size(); i++){
+            if(!schedulers.get(i).isEmpty()){
+                currentScheduler = i;
+                break;
+            }
+        }
     }
     
    
@@ -47,13 +57,37 @@ public class MFQ extends Scheduler{
     public void getNext(boolean cpuEmpty) {
         //Suggestion: now that you know on which scheduler a process is, you need to keep advancing that scheduler. If it a preemptive one, you need to notice the changes
         //that it may have caused and verify if the change is coherent with the priority policy for the queues.
-  
+        if(currentScheduler==1){
+            defineCurrentScheduler();
+        }
+        if(currentScheduler!=-1){
+            schedulers.get(currentScheduler).getNext(cpuEmpty);
+            defineCurrentScheduler();
+        }
     }
     
     @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
+    public void newProcess(boolean cpuEmpty) {
+        defineCurrentScheduler();
+        if(currentScheduler!=-1){
+            schedulers.get(0).newProcess(cpuEmpty);
+        }
+    } 
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+    public void IOReturningProcess(boolean cpuEmpty) {
+        defineCurrentScheduler();
+        if(currentScheduler!=-1){
+            schedulers.get(0).IOReturningProcess(cpuEmpty);
+        }
+    }
+    public void downgradeProcess(Process p, int currentLevel) {
+        if (currentLevel < schedulers.size() - 1) {
+            schedulers.get(currentLevel + 1).addProcess(p);
+        } else {
+            schedulers.get(currentLevel).addProcess(p);
+        }
+        defineCurrentScheduler();
+    }
+
 }
