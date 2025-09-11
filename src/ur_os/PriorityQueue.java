0f1723fd1b6ac1359c9f79c5cdebf850ae48ad93
QueenR1014 <juanreinareyes@hotmail.com>
2025-09-10 20:37:00 -0500

    PriorityQueue non preemptive

--- src/ur_os/PriorityQueue.java
+++ src/ur_os/PriorityQueue.java
@@ -1,120 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ur_os;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-
-/**
- *
- * @author prestamour
- */
-
-//Implementation is preemptive
-public class PriorityQueue extends Scheduler{
+//Implementation non preemptive to avoid parent to child queue communication, also just relies 
+//on quantum to liberate their cpu space.
+public class PriorityQueue extends Scheduler {
 
     private int currentScheduler;
     private ArrayList<Scheduler> schedulers;
-    
-    //----------------- 1. Single priority queues
+
+    // 1. Single priority queue (default)
     PriorityQueue(OS os) {
         super(os);
         this.schedulers = new ArrayList<>();
-        //this.threshold = new ArrayList<>();
-        //this.starvationCounter = new ArrayList<>();
         this.currentScheduler = -1;
 
-        // default: one RR
         Scheduler rr = new RoundRobin(os, 5);
         schedulers.add(rr);
-        /*threshold.add(10);
-        starvationCounter.add(0);*/
-        currentScheduler = 0; //default value for single queue
+        currentScheduler = 0;
     }
-    
-    //----------------- 2. Multiple queues, default thresholds
+
+    // 2. Multiple queues, default thresholds
     PriorityQueue(OS os, Scheduler... s) {
-        this(os); // initialize lists
+        this(os);
         schedulers.addAll(Arrays.asList(s));
-
-        /*int defaultStarvation = 10;
-        for (int i = 0; i < s.length; i++) {
-            threshold.add(defaultStarvation);
-            starvationCounter.add(0);
-        }*/
-
         this.currentScheduler = (s.length > 0) ? 0 : -1;
     }
 
-    //----------------- 3. Multiple queues, specified thresholds
-    /*PriorityQueue(OS os, ArrayList<Scheduler> s, ArrayList<Integer> t){
-        super(os);
-        //check every queue has it's starvation limit
-        if(s.size() != t.size()){
-            throw new IllegalArgumentException("Ammount of schedulers not equal to ammount of thresholds");
-        }
-        this.schedulers = s;
-        this.threshold = t;
-        this.starvationCounter = new ArrayList<>();
-        //initialize counters
-        for(int i = 0; i < t.size();i++){
-            starvationCounter.add(0);
-        }
-
-        this.currentScheduler = (s.size() > 0) ? 0: 1;
-    }
-    */
-    
     @Override
-    public void addProcess(Process p){
+    public void addProcess(Process p) {
         int prio = p.getPriority();
 
-        if(prio > schedulers.size() - 1){
-            //Go to last scheduler 
+        if (prio > schedulers.size() - 1) {
             prio = schedulers.size() - 1;
         }
-        //Let the priority be the queue a process is in
-        Scheduler s = schedulers.get(prio);
-        
-        // Check if current process preempts the loaded process in cpu
-        if(!os.isCPUEmpty()){
-            Process running = os.getProcessInCPU();
-            if(running != null){
-                //smaller index => higher priority
-                if(prio < running.getPriority()){
-                    //No comentar lo de abajo si buggea
-                    //s.removeProcess(p);
-                    
-                    os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, running);
-                    addContextSwitch(); //context switch for interruption
-                    
-                    os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
-                    addContextSwitch();
-                }
-            }
-        }
 
-        //if no preemption needed add it to the RQ
+        Scheduler s = schedulers.get(prio);
         s.addProcess(p);
-        
-        //schedule if cpu empty
-        if(os.isCPUEmpty()){
+
+        if (os.isCPUEmpty()) {
             getNext(true);
         }
     }
-    
-    int defineCurrentScheduler(){
-        //This method is suggested to help you find the scheduler that should be the next in line to provide processes... perhaps the one with process in the queue?
-        
-        int topPriority = -1; //function will return -1 if all queues are empty
+
+    int defineCurrentScheduler() {
+        int topPriority = -1;
         int i = 0;
 
-        while(i < schedulers.size()){
+        while (i < schedulers.size()) {
             Scheduler s = schedulers.get(i);
-            if(!s.processes.isEmpty()){
+            if (!s.processes.isEmpty()) {
                 topPriority = i;
                 break;
             }
@@ -122,50 +57,33 @@ public class PriorityQueue extends Scheduler{
         }
         return topPriority;
     }
-    
-   
+
     @Override
     public void getNext(boolean cpuEmpty) {
-        //Define if any higher priority queue is loading processes 
+        if (!cpuEmpty) return;
+
         int topPrio = defineCurrentScheduler();
 
-        if(topPrio == -1){
-            //all queues empty, no process has to be loaded in cpu
-            return;
-        }
-        
-        //If CPU is empty, load the highest priority queue
-        if(cpuEmpty){
-            Scheduler top = schedulers.get(topPrio);
-            top.getNext(true);
-            currentScheduler = topPrio;
+        if (topPrio == -1) {
             return;
         }
 
-        //If CPU is not empty, check if we should preempt
-        Process running = os.getProcessInCPU();
-        int runningPrio = running.getPriority();
-
-        if(topPrio < runningPrio){
-            //the next process has a higher priority
-            os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, running);
-            addContextSwitch();
-
-            Scheduler top = schedulers.get(topPrio);
-            top.getNext(true);
-            currentScheduler = topPrio;
-        }else{
-            //No higher priority than current process -> let current queue handle
-            Scheduler s = schedulers.get(runningPrio);
-            s.getNext(cpuEmpty);
-            currentScheduler = runningPrio;
-        }
+        Scheduler top = schedulers.get(topPrio);
+        top.getNext(true);
+        currentScheduler = topPrio;
     }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
+
+    public int getIndexOf(Scheduler s) {
+        return schedulers.indexOf(s);
+    }
+
+    public int getTopPriority() {
+        return defineCurrentScheduler();
+    }
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+    public void newProcess(boolean cpuEmpty) {}
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {}
 }
